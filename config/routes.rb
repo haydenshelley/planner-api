e766df5c4589206268a6dfefab2b01e0f32be499
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-17 10:49:47 -0800

    create plan/user associations

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#delete"
 
+  get "/plans" => "plans#index"
+  post "/plans" => "plans#create"
+  get "/plans/:id" => "plans#show"
+  patch "/plans/:id" => "plans#update"
+  delete "/plans/:id" => "plans#delete"
+
   post "/sessions" => "sessions#create"
 end
