778014c2faba05d9c5abaa6665f641634e9febef
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-17 10:53:15 -0800

    plan create action

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,16 @@
 class PlansController < ApplicationController
+  def create
+    @plan = Plan.new(
+      user_id: current_user.id,
+      title: params[:title],
+      date: params[:date],
+      time: params[:time],
+      info: params[:info],
+    )
+    if @plan.save
+      render json: { message: "Plan created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
